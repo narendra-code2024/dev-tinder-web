1e9705364bc0fef7a89549bd76f437acfecb350c
narendra-code2024 <im.narendra.k@gmail.com>
2025-07-12 18:44:32 +0530

    added react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import NavBar from "./components/NavBar";
+import { BrowserRouter, Routes, Route } from "react-router";
+import Body from "./components/Body";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
 
 function App() {
 	return (
 		<>
-			<NavBar />
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Body />}>
+						<Route path="/login" element={<Login />}></Route>
+            <Route path="/profile" element={<Profile />}></Route>
+					</Route>
+				</Routes>
+			</BrowserRouter>
 		</>
 	);
 }
